e81ee8d2a87aa8c0ac35bb78344c9cb6f79f14a6
AKornienko123 <71881413+AKornienko123@users.noreply.github.com>
2020-12-06 23:31:10 +0200

    Add files via upload
    Це Корнієнко Андрій 2 курс 1 група комп. мат. домашнє було dz4: 4г; 24б

---
+++ 24б.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <cmath>
+
+
+int main()
+{
+    int i = 1;
+    float y, n, z;
+    float a;
+
+    printf("n: ");
+    scanf_s("%f", &n);
+
+    printf("y: ");
+    scanf_s("%f", &y);
+    
+    a = y;
+    if (abs(a) >= 1) {
+       a = a;
+    }
+
+    else {
+        a = 2;
+    }
+
+    while (i <= n-1) {
+
+        printf("y: ");
+        scanf_s("%f", &y);
+
+        if (abs(y) >= 1) {
+            z = y;
+        }
+
+        else {
+            z = 2;
+        }
+        
+        if (a <= z) {
+            a = a;
+        }
+        else {
+            a = z;
+        }
+
+        i += 1;
+
+    }
+
+    printf("a=%f", a);
+
+}
