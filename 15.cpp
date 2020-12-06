80d8e1ebbc3dbadffd3dde188e202fcc737e68dd
AKornienko123 <71881413+AKornienko123@users.noreply.github.com>
2020-12-06 23:34:26 +0200

    dz5: 35б; 15
    Це Корнієнко Андрій 2 курс 1 група комп. мат. домашнє було dz5: 35б; 15

---
+++ 15.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <cmath>
+
+
+int main()
+{
+    
+    float y, a;
+    int i;
+
+    i = 0;
+    printf("y: ");
+    scanf_s("%f", &y);
+    a = y;
+    
+    while (y != 0) {
+
+        printf("y: ");
+        scanf_s("%f", &y);
+        if (a > 0 and y < 0) {
+            i += 1;
+
+
+        }
+        else if (a < 0 and y > 0) {
+            i += 1;
+        }
+        a = y;
+
+        }
+
+    printf("i=: %i ", i);
+    }
+
+    
---
+++ 35b.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <cmath>
+
+
+int main()
+{
+
+    int a, b, c, n, d, f;
+    a = -99;
+    b = a;
+    c = b;
+    n = 3;
+    
+    while (a <=0 ) {
+        
+        
+        if (n == 3) {
+            a = a + b + c + 200;
+            n += 1;
+            f = a;
+        }
+        
+       
+        if (n <= 5 and n != 3) {
+            d = a;
+            a = a + b + c + 200;
+            b = d;
+            n += 1;
+        }
+        
+        else {
+            c = f;
+            d = a;
+            a = a + b + c + 200;
+            f = b;
+            b = d;
+            n += 1;
+        }
+        
+        
+        }
+    printf("a=:%i", a);
+    
+    }
+
+    
+
+
