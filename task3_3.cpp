5de7bcdfdf3f1fa6891499d6c8b96a7165c82523
AKornienko123 <71881413+AKornienko123@users.noreply.github.com>
2020-12-07 00:11:45 +0200

    Classwork 3,5,6,10-13,30
    Це Корнієнко Андрій 2 курс 1 група комп. мат. номери, які були на парі:  3,5,6,10-13,30

---
+++ task3_3.cpp
@@ -0,0 +1,18 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+    float a, b = 1;
+    int i = 1;
+    printf("a:");
+    scanf_s("%f", &a);
+    
+    while (i != a+1) {
+        b = b * i;
+        i += 1;
+    }
+    printf("b=:%f",b);
+}
+
