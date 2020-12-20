3abeea996ba7f878ee5fd89017a291e0d05f4603
AKornienko123 <71881413+AKornienko123@users.noreply.github.com>
2020-12-21 00:08:13 +0200

    dz 7: 17
    Це Корнієнко Андрій 2 курс 1 група комп. мат. домашнє було dz 7: 17

---
+++ 17.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[10] = { 1, 3, 2, 4, 8, 11, 3, 5, 8, 7 };
+    int max = arr[0], min = arr[0], q = 0, a;
+
+    for (int i = 0; i < 10; ++i) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    for (int i = 0; i < 10; ++i) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    q = (min + max) / 2;
+    for (int i = 0; i < 10; i++)
+    {
+        if (abs(arr[i] - q) <= min)
+        {
+            min = abs(arr[i] - q);
+            a = arr[i];
+        }
+    }
+    cout << a << std::endl;
+    return 0;
+}
