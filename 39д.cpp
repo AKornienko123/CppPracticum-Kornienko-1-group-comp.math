ca751869d74eb475b026ad1392a8f5a3b8db7261
AKornienko123 <71881413+AKornienko123@users.noreply.github.com>
2020-12-21 00:00:43 +0200

    dz 6: 39д; 46в
    Це Корнієнко Андрій 2 курс 1 група комп. мат. домашнє було dz5: 35б; 15

---
+++ 39д.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	float a0 = 1,t = 1,sum = 0, ak, n;
+	std::cin >> n;
+	for (float k = 1; k <= n; k++) {
+		
+		ak = k * a0 + 1 / k;
+		a0 = ak;
+		t *= k;
+		sum +=  ((pow(2,k) / t) * ak);
+		std::cout << "sum= " << sum;
+	}
+	
+	return 0;
+
+}
