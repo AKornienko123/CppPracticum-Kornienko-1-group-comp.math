60695c6cf62bdcb10a89fd45a50f1f9a9c2c32df
AKornienko123 <71881413+AKornienko123@users.noreply.github.com>
2020-12-22 07:59:25 +0200

    Classwork К.Р:3.1 1-3,6-7
    Це Корнієнко Андрій 2 курс 1 група комп. мат. номери, які були на парі: 1-3,6-7

---
+++ tasks3_2.1(a).cpp
@@ -0,0 +1,35 @@
+﻿#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+	char a[10] = "12ab-cd+3";
+	char b[10];
+	int k = 0;
+	for (int i = 0; i < 10; i++)
+	{
+		if (a[i] >= '0' && a[i] <= '9')
+		{
+			continue;
+		}
+		b[k] = a[i];
+		k++;
+		if (a[i] == '+')
+		{
+			b[k] = a[i];
+			k++;
+		}
+		if (a[i] == '-')
+		{
+			b[k] = a[i];
+			k++;
+		}
+	}
+
+	for (int i = 0; i < strlen(b); i++)
+	{
+		printf("%c", b[i]);
+	}
+
+	getchar();
+}
